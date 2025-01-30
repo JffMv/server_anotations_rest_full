eb522014015dee8b8191f552a4eaef97d51df7d4
Jefferson Mesa <mesajefer2@gmail.com>
2025-01-30 14:12:18 -0500

    add readme and test

---
+++ README.md
@@ -0,0 +1,73 @@
+# Server Reader Anything
+
+Use API´s for bring files with extentions .png .html .css .js save in the local disk.
+
+
+## Getting Started
+
+For clone this repository you use this command:
+ ```
+ git clone https://github.com/JffMv/Server_reader.git
+ ```
+### Prerequisites
+
+Have installed:
+maven 3.9.6 - [Install Maven](https://maven.apache.org/download.cgi#Installation)
+
+git 2.44 - [Install Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+
+java 21 - [Install Java](https://www.oracle.com/co/java/technologies/downloads/)
+
+## Deployment
+
+The process were building project maven with the command:
+
+```
+mvn archetype:generate -DgroupId=org.example -DartifactId=CALCULATOR -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+```
+
+Then we have verify the class App.java and the pom.xml, run the project with:
+
+```
+mvn package
+```
+
+
+
+
+
+## Architecture
+![img_1.png](pictures_of_readme/img_1.png)
+### HttpServer:
+Is one server that host several services API´s
+
+### Form HTML
+Host a simple form and functions javascript. Those functions call API´s
+
+## Test
+if you want run of test use this command:
+```
+mvn test
+```
+Test of server and API´s work
+![img.png](pictures_of_readme/img.png)
+
+## How it see
+![img_2.png](pictures_of_readme/img_2.png)
+
+## Built With
+
+* [Java](https://www.java.com/es/) - The language used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [React](https://reactjs.org/) - Frontend framework
+* [Git](http://git-scm.com/) - Version Control System
+
+
+## Authors
+
+* **Yeferson Mesa**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENCE.md](LICENCE) file for details
---
+++ src/test/java/co/edu/escuelaing/HttpServerTest.java
@@ -0,0 +1,97 @@
+package co.edu.escuelaing;
+
+import org.junit.jupiter.api.*;
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HttpServerTest {
+
+    private static final int PORT = 8080;
+    private static Thread serverThread;
+
+    // Iniciar el servidor en un hilo separado antes de cada prueba
+    @BeforeAll
+    static void startServer() {
+        serverThread = new Thread(() -> {
+            String[] args = {};  // Los args pueden ser vacíos
+            HttpServer.main(args);
+        });
+        serverThread.start();
+
+        // Esperamos un poco para asegurarnos de que el servidor esté en funcionamiento
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Detener el servidor después de cada prueba
+    @AfterAll
+    static void stopServer() {
+        // Lógica para detener el servidor si fuera necesario, aunque en este caso solo cerramos el hilo
+        serverThread.interrupt();
+    }
+
+    // Prueba para el endpoint /
+    @Test
+    void testRootEndpoint() throws IOException {
+        // Realizamos una solicitud GET al endpoint "/"
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + PORT + "/").openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        // Verificamos que la respuesta sea 200 OK
+        assertEquals(200, responseCode, "La respuesta del servidor no es 200 OK");
+    }
+
+    // Prueba para el endpoint /imagen
+    @Test
+    void testImagenEndpoint() throws IOException {
+        // Realizamos una solicitud GET al endpoint "/imagen"
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + PORT + "/imagen").openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        // Verificamos que la respuesta sea 200 OK
+        assertEquals(200, responseCode, "La respuesta del servidor no es 200 OK");
+    }
+
+    // Prueba para el endpoint /html
+    @Test
+    void testHtmlEndpoint() throws IOException {
+        // Realizamos una solicitud GET al endpoint "/html"
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + PORT + "/html").openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        // Verificamos que la respuesta sea 200 OK
+        assertEquals(200, responseCode, "La respuesta del servidor no es 200 OK");
+    }
+
+    // Prueba para el endpoint /css
+    @Test
+    void testCssEndpoint() throws IOException {
+        // Realizamos una solicitud GET al endpoint "/css"
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + PORT + "/css").openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        // Verificamos que la respuesta sea 200 OK
+        assertEquals(200, responseCode, "La respuesta del servidor no es 200 OK");
+    }
+
+    // Prueba para el endpoint /js
+    @Test
+    void testJsEndpoint() throws IOException {
+        // Realizamos una solicitud GET al endpoint "/js"
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + PORT + "/js").openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        // Verificamos que la respuesta sea 200 OK
+        assertEquals(200, responseCode, "La respuesta del servidor no es 200 OK");
+    }
+}
